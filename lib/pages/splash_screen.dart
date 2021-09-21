0cede3e8677d5a5dcf43b18057d319e868221426
Your Name <you@example.com>
2021-09-21 12:20:45 +0600

    New files

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Container(
+        height: _height,
+        width: _width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "NewsToday",
+              style: TextStyle(
+                fontSize: 36,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CircularProgressIndicator(
+              backgroundColor: Colors.amber,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
